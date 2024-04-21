02f43274d428bf1151b54617bfb41a1d1557cb9e
lamp-cap <131742619+lamp-cap@users.noreply.github.com>
2024-04-21 13:52:37 +0800

    commit /model

---
+++ DX12Project1.cpp
@@ -0,0 +1,32 @@
+﻿
+#include "./Source/App.h"
+
+const int gNumFrameResources = 3;
+const bool mFrameLimit = true;
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+    try
+    {
+        LampApp theApp(hInstance);
+        if (!theApp.Initialize())
+            return 0;
+
+        return theApp.Run();
+    }
+    catch (DxException& e)
+    {
+        MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+        return 0;
+    }
+}
